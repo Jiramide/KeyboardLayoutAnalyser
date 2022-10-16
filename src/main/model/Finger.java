bb7d96cb39859e9a509aaaeddf60637cd5765149
Jade Piramide <jadepiramidepogi@gmail.com>
2022-10-15 17:01:21 -0700

    Added methods for serialization and deserialization for Finger.

--- src/main/model/Finger.java
+++ src/main/model/Finger.java
@@ -1,20 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 public enum Finger {
 
-    LEFT_PINKY,
-    LEFT_RING,
-    LEFT_MIDDLE,
-    LEFT_INDEX,
-    LEFT_THUMB,
+    LEFT_PINKY(0),
+    LEFT_RING(1),
+    LEFT_MIDDLE(2),
+    LEFT_INDEX(3),
+    LEFT_THUMB(4),
 
-    RIGHT_PINKY,
-    RIGHT_RING,
-    RIGHT_MIDDLE,
-    RIGHT_INDEX,
-    RIGHT_THUMB;
+    RIGHT_PINKY(5),
+    RIGHT_RING(6),
+    RIGHT_MIDDLE(7),
+    RIGHT_INDEX(8),
+    RIGHT_THUMB(9);
 
-    // effects: determines whether the given finger belongs
+    private static final Finger[] indexToFinger = {
+            LEFT_PINKY, LEFT_RING, LEFT_MIDDLE, LEFT_INDEX, LEFT_THUMB,
+            RIGHT_PINKY, RIGHT_RING, RIGHT_MIDDLE, RIGHT_INDEX, RIGHT_THUMB};
+
+    private int fingerIndex;
+
+    Finger(int fingerIndex) {
+        this.fingerIndex = fingerIndex;
+    }
+
+    // EFFECTS: returns the finger with the given index (for parsing from file)
+    public static Finger fromFingerIndex(int fingerIndex) {
+        return indexToFinger[fingerIndex];
+    }
+
+    // EFFECTS: determines whether the given finger belongs
     //          on the left hand
     public boolean isLeftHand() {
         switch (this) {
@@ -27,6 +44,11 @@ public enum Finger {
             default:
                 return false;
         }
+    }
+
+    // EFFECTS: returns the finger index
+    public int getFingerIndex() {
+        return fingerIndex;
     }
 
 }
