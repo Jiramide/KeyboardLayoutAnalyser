88b84b64906c30ed17bfa347d2df02584b7ea775
Jade Piramide <jadepiramidepogi@gmail.com>
2022-10-15 17:14:19 -0700

    Added tests for the serialization-related methods for Finger.

--- src/test/model/FingerTest.java
+++ src/test/model/FingerTest.java
@@ -9,7 +9,7 @@ import model.Finger;
 public class FingerTest {
 
     @Test
-    void isLeftHandTest() {
+    public void isLeftHandTest() {
         assertTrue(Finger.LEFT_PINKY.isLeftHand());
         assertTrue(Finger.LEFT_RING.isLeftHand());
         assertTrue(Finger.LEFT_MIDDLE.isLeftHand());
@@ -21,6 +21,34 @@ public class FingerTest {
         assertFalse(Finger.RIGHT_MIDDLE.isLeftHand());
         assertFalse(Finger.RIGHT_INDEX.isLeftHand());
         assertFalse(Finger.RIGHT_THUMB.isLeftHand());
+    }
+
+    @Test
+    public void testFromIndex() {
+        assertEquals(Finger.LEFT_PINKY, Finger.fromFingerIndex(0));
+        assertEquals(Finger.LEFT_RING, Finger.fromFingerIndex(1));
+        assertEquals(Finger.LEFT_MIDDLE, Finger.fromFingerIndex(2));
+        assertEquals(Finger.LEFT_INDEX, Finger.fromFingerIndex(3));
+        assertEquals(Finger.LEFT_THUMB, Finger.fromFingerIndex(4));
+        assertEquals(Finger.RIGHT_PINKY, Finger.fromFingerIndex(5));
+        assertEquals(Finger.RIGHT_RING, Finger.fromFingerIndex(6));
+        assertEquals(Finger.RIGHT_MIDDLE, Finger.fromFingerIndex(7));
+        assertEquals(Finger.RIGHT_INDEX, Finger.fromFingerIndex(8));
+        assertEquals(Finger.RIGHT_THUMB, Finger.fromFingerIndex(9));
+    }
+
+    @Test
+    public void testGetIndex() {
+        assertEquals(0, Finger.LEFT_PINKY.getFingerIndex());
+        assertEquals(1, Finger.LEFT_RING.getFingerIndex());
+        assertEquals(2, Finger.LEFT_MIDDLE.getFingerIndex());
+        assertEquals(3, Finger.LEFT_INDEX.getFingerIndex());
+        assertEquals(4, Finger.LEFT_THUMB.getFingerIndex());
+        assertEquals(5, Finger.RIGHT_PINKY.getFingerIndex());
+        assertEquals(6, Finger.RIGHT_RING.getFingerIndex());
+        assertEquals(7, Finger.RIGHT_MIDDLE.getFingerIndex());
+        assertEquals(8, Finger.RIGHT_INDEX.getFingerIndex());
+        assertEquals(9, Finger.RIGHT_THUMB.getFingerIndex());
     }
 
 }
