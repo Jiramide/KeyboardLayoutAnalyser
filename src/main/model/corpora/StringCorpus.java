09ad4fe7e6ed1c1dd953b608310a88b21da00878
Jade Piramide <jadepiramidepogi@gmail.com>
2022-10-16 14:54:48 -0700

    Lifted previous StringCorpus behaviour into a StringCorpusReader class

--- src/main/model/corpora/StringCorpus.java
+++ src/main/model/corpora/StringCorpus.java
@@ -3,30 +3,43 @@ package model.corpora;
 public class StringCorpus implements Corpus {
 
     private String content;
-    private int index;
     private int contentSize;
 
     // EFFECTS: constructs a StringCorpus with the string as the content
     public StringCorpus(String content) {
         this.content = content;
-        this.index = 0;
         this.contentSize = content.length();
     }
 
-    // REQUIRES: !isFinished()
-    // MODIFIES: this
-    // EFFECTS: "consumes" the next character from the corpus, returning it and moving onto the next
-    //          character. Once a character is consumed, it cannot be returned to again.
-    public char consume() {
-        char consumedChar = content.charAt(index);
-        index += 1;
-
-        return consumedChar;
+    // EFFECTS: Creates a CorpusReader responsible for going through corpus data
+    public StringCorpusReader createCorpusReader() {
+        return null;
     }
 
-    // EFFECTS: determines if the corpus is finished (i.e. out of content)
-    public boolean isFinished() {
-        return index >= contentSize;
+    private class StringCorpusReader implements CorpusReader {
+
+        private int index;
+
+        public StringCorpusReader() {
+
+        }
+
+        // REQUIRES: !isFinished()
+        // MODIFIES: this
+        // EFFECTS: "consumes" the next character from the corpus, returning it and moving onto the next
+        //          character. Once a character is consumed, it cannot be returned to again.
+        public char consume() {
+            char consumedChar = content.charAt(index);
+            index += 1;
+
+            return consumedChar;
+        }
+
+        // EFFECTS: determines if the corpus is finished (i.e. out of content)
+        public boolean isFinished() {
+            return index >= contentSize;
+        }
+
     }
 
 }
