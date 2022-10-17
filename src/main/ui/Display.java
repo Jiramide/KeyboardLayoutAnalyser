5fc02410c48e7315500381f418ee6d33fc979c38
Jade Piramide <jadepiramidepogi@gmail.com>
2022-10-16 20:43:01 -0700

    Added description to Display

--- src/main/ui/Display.java
+++ src/main/ui/Display.java
@@ -5,15 +5,21 @@ import model.KeyboardGeometry;
 public class Display<T> {
 
     private String name;
+    private String description;
     private T associatedObject;
 
-    public Display(String name, T object) {
+    public Display(String name, String description, T object) {
         this.name = name;
+        this.description = description;
         this.associatedObject = object;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public T getAssociatedObject() {
