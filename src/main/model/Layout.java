10efebcab139f08125cb13e6f9a07982b1f1be51
Jade Piramide <jadepiramidepogi@gmail.com>
2022-10-26 22:30:46 -0700

    Implement Layout according to spec

--- src/main/model/Layout.java
+++ src/main/model/Layout.java
@@ -10,17 +10,17 @@ public class Layout extends Nameable {
     // EFFECTS: creates a layout with the given name, description; determines the actual permutation through
     //          the given 'layout' string
     public Layout(String name, String description, String layout) {
-
+        super(name, description);
     }
 
     // EFFECTS: finds the index of the given key in the layout. if the key is not found, return -1.
     public int getIndexOfKey(char key) {
-        return -1;
+        return layout.indexOf(key);
     }
 
     // EFFECTS: returns the layout string
     public String getLayoutString() {
-        return null;
+        return layout;
     }
 
 }
