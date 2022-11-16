716d2fcfb784a48b7ad03036c2033d2b6de0d134
Jade Piramide <jadepiramidepogi@gmail.com>
2022-11-15 23:59:04 -0800

    Create new Main that instantiates the MainWindow

---
+++ src/main/ui/gui/Main.java
@@ -0,0 +1,12 @@
+package ui.gui;
+
+/*
+ * The entry point of the program. Just instantiates the GUI
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+
+}
