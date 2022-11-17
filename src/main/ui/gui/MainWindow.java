f2fb4f58704d7e2635adeaabcbc7fb22718d7e45
Jade Piramide <jadepiramidepogi@gmail.com>
2022-11-16 21:30:06 -0800

    Clean up the MainWindow class by extracting some methods out

--- src/main/ui/gui/MainWindow.java
+++ src/main/ui/gui/MainWindow.java
@@ -1,6 +1,7 @@
 package ui.gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 import java.awt.event.ActionEvent;
@@ -14,6 +15,7 @@ import model.Layout;
 import model.Tournament;
 import model.corpora.Corpus;
 
+import model.corpora.StringCorpus;
 import ui.gui.panels.*;
 
 /*
@@ -26,58 +28,72 @@ public class MainWindow {
 
     public static final int WIDTH = 600;
     public static final int HEIGHT = 400;
-
+    public static final int SIDE_PADDING = 5;
 
     private JFrame frame = new JFrame(TITLE);
+    private JPanel mainPanel = new JPanel();
     private CardLayout cardLayout = new CardLayout();
-
-    /*
-    private SelectionPanel selectionPanel;
-
-    private CorporaMainPanel corporaMainPanel;
-    private CorporaCreationPanel corporaCreationPanel;
-    private CorporaViewPanel corporaViewPanel;
-
-    private KeyboardMainPanel keyboardMainPanel;
-    private KeyboardConstructionPanel keyboardConstructionPanel;
-    private KeyboardViewPanel keyboardViewPanel;
-
-    private TournamentPanel tournamentPanel;
-     */
-
 
     private List<Corpus> corpora = new ArrayList<>();
     private List<KeyboardGeometry> keyboardGeometries = new ArrayList<>();
     private List<Layout> layouts = new ArrayList<>();
 
     public MainWindow() {
-        frame.setLayout(cardLayout);
+        setUpContentPanel();
+        setUpPaddedPanel();
 
-        frame.add(new SelectionPanel(this), PAGE_TO_ID_MAP.get(Page.Selection));
-
-        frame.setSize(WIDTH, HEIGHT);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addPage(Page.Selection, new SelectionPanel(this));
+        addPage(Page.CorporaMain, new CorporaMainPanel(this));
+        addPage(Page.KeyboardMain, new KeyboardMainPanel(this));
+        addPage(Page.Tournament, new TournamentPanel(this));
 
         goTo(Page.Selection);
     }
 
-    private void createPanel() {
+    private void setUpPaddedPanel() {
+        frame.setLayout(new BorderLayout());
 
+        Component padding = Box.createRigidArea(new Dimension(SIDE_PADDING, SIDE_PADDING));
+        frame.add(padding, BorderLayout.PAGE_START);
+        frame.add(padding, BorderLayout.BEFORE_LINE_BEGINS);
+        frame.add(padding, BorderLayout.PAGE_END);
+        frame.add(padding, BorderLayout.AFTER_LINE_ENDS);
+
+        frame.add(mainPanel, BorderLayout.CENTER);
+
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void createPages() {
-        /*
-        corporaMainPanel = new CorporaMainPanel();
-        frame.add(corporaMainPanel, PAGE_TO_ID_MAP.get(Page.CorporaMain)
-        */
+    private void setUpContentPanel() {
+        mainPanel.setLayout(cardLayout);
+
+        mainPanel.setVisible(true);
     }
+
+    public List<Corpus> getCorpora() {
+        return corpora;
+    }
+
+    public List<KeyboardGeometry> getKeyboardGeometries() {
+        return keyboardGeometries;
+    }
+
+    public List<Layout> getLayouts() {
+        return layouts;
+    }
+
+    public void addPage(Page page, JComponent panel) {
+        mainPanel.add(panel, PAGE_TO_ID_MAP.get(page));
+    }
+
 
     // MODIFIES: this
     // EFFECTS: shows the page panel associated with the page enum
     public void goTo(Page destination) {
         System.out.println("going to " + PAGE_TO_ID_MAP.get(destination));
-        cardLayout.show(frame.getContentPane(), PAGE_TO_ID_MAP.get(destination));
+        cardLayout.show(mainPanel, PAGE_TO_ID_MAP.get(destination));
     }
 
     // EFFECTS: creates a button which, upon clicked, will go to the page passed
