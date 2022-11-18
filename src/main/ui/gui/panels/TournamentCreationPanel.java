49ff3b96b1712f599e6951f0bc7efb68dc657474
Jade Piramide <jadepiramidepogi@gmail.com>
2022-11-18 00:14:20 -0800

    Implement the inner class according to spec

--- src/main/ui/gui/panels/TournamentCreationPanel.java
+++ src/main/ui/gui/panels/TournamentCreationPanel.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import model.KeyboardGeometry;
@@ -35,7 +36,7 @@ public class TournamentCreationPanel extends JPanel {
     private JLabel effortModelText;
     private JComboBox<String> effortModelChooser;
     private JScrollPane keyboards;
-    private List<KeyboardInformationPanel> keyboardsInfo;
+    protected List<KeyboardInformationPanel> keyboardsInfo;
     private JComboBox<String> keyboardChooser;
     private JComboBox<String> layoutChooser;
     private JButton addKeyboard;
@@ -103,9 +104,8 @@ public class TournamentCreationPanel extends JPanel {
      * A class that represents an information panel about the keyboard; allows for
      * interaction buttons like removal
      */
-    private static class KeyboardInformationPanel extends JPanel {
+    private class KeyboardInformationPanel extends JPanel {
 
-        private TournamentCreationPanel parent;
         private KeyboardGeometry geometry;
         private Layout layout;
 
@@ -116,42 +116,96 @@ public class TournamentCreationPanel extends JPanel {
         private JButton removeButton;
 
         // EFFECTS: creates a KeyboardInformationPanel with the given geometry and layout
-        public KeyboardInformationPanel(TournamentCreationPanel parent, KeyboardGeometry geometry, Layout layout) {
+        public KeyboardInformationPanel(KeyboardGeometry geometry, Layout layout) {
+            super();
 
+            this.geometry = geometry;
+            this.layout = layout;
+
+            setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+            setUpGeometryLabel();
+            setupLayoutLabel();
+            setUpInteractionButtons();
+            layoutComponents();
+            setVisible(true);
         }
 
         // MODIFIES: this
         // EFFECTS: lays out the components in their proper positions
         private void layoutComponents() {
-
+            add(geometryLabel);
+            add(Box.createRigidArea(new Dimension(10, 0)));
+            add(layoutLabel);
+            add(Box.createHorizontalGlue());
+            add(interactionButtons);
         }
 
         // MODIFIES: this
         // EFFECTS: sets up the label for the geometry
         private void setUpGeometryLabel() {
-
+            geometryLabel = new JLabel(geometry.getName());
         }
 
         // MODIFIES: this
         // EFFECTS: sets up the label for the layout
         private void setupLayoutLabel() {
-
+            layoutLabel = new JLabel(layout.getName());
         }
 
         // MODIFIES: this
         // EFFECTS: sets up the interaction buttons
         private void setUpInteractionButtons() {
+            interactionButtons = new JPanel();
+            interactionButtons.setLayout(new BoxLayout(interactionButtons, BoxLayout.LINE_AXIS));
 
+            viewButton = createViewButton();
+            removeButton = createRemoveButton();
+
+            interactionButtons.add(viewButton);
+            interactionButtons.add(Box.createRigidArea(new Dimension(10, 0)));
+            interactionButtons.add(removeButton);
+
+            interactionButtons.setVisible(true);
         }
 
         // EFFECTS: creates the view button
         private JButton createViewButton() {
-            return null;
+            viewButton = new JButton("👁    View");
+            viewButton.setEnabled(false);
+            viewButton.setVisible(true);
+
+            return viewButton;
         }
 
         // EFFECTS: creates the remove button
         private JButton createRemoveButton() {
-            return null;
+            removeButton = new JButton("➖    Remove");
+            removeButton.setVisible(true);
+
+            KeyboardInformationPanel panel = this;
+
+            removeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Iterator<KeyboardInformationPanel> iterator = keyboardsInfo.listIterator();
+
+                    while (iterator.hasNext()) {
+                        KeyboardInformationPanel next = iterator.next();
+
+                        if (panel == next) {
+                            iterator.remove();
+                            keyboards.remove(panel);
+
+                            break;
+                        }
+                    }
+
+                    removeButton.setEnabled(false);
+                }
+            });
+
+            return removeButton;
         }
     }
 }
