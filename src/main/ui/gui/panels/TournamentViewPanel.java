226bbfbc475bb39c15378b65ee3f9152b09ece1d
Jade Piramide <jadepiramidepogi@gmail.com>
2022-11-18 00:14:32 -0800

    Create a TournamentViewPanel

---
+++ src/main/ui/gui/panels/TournamentViewPanel.java
@@ -0,0 +1,18 @@
+package ui.gui.panels;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.Tournament;
+import ui.gui.MainWindow;
+
+import static ui.gui.MainWindow.Page;
+
+public class TournamentViewPanel extends JPanel {
+
+    public TournamentViewPanel(TournamentPanel parent, MainWindow mainWindow) {
+
+    }
+}
